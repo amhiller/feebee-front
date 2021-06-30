a7b77e5d8ddbb35e169fd7a4977169f24deede77
Austin Dev <amhiller96@gmail.com>
2021-06-30 13:10:43 -0400

    First push for front end

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo1.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          FeeBee - Construction Pending
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coming Soon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
